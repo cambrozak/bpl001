7fe098fe48b1969315654026e8096db2eecf4ad6
cambrozak <carlos.ambrozak@gmail.com>
2016-02-28 16:46:07 -0800

    removed lcd cycle from lcd identy code

--- session-01/identity_lcd/identity_lcd.ino
+++ session-01/identity_lcd/identity_lcd.ino
@@ -53,24 +53,5 @@ void displayIdentity() {
 
   // wait three seconds
   delay(3000);
-
-  // cycle thru characters
-  cycle();
-}
-
-void cycle() {
-  for (int i = 0; i < 256; i++) {
-    lcd.clear();
-    lcd.print(i);
-    lcd.print("  ");
-    lcd.print((char)i);
-    lcd.print((char)i);
-    lcd.gotoXY(0, 1);
-    lcd.print(i);
-    lcd.print("  ");
-    lcd.print((char)i);
-    lcd.print((char)i);
-    delay(1000);
-  }  
 }
 
